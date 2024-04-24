cfd2fc4058af0ef6e7788a690d7a42d23ae6ff9b
francescofalcone05 <ciccio_97@live.it>
2024-04-24 18:31:48 +0300

    stampo -in-pagina

--- js/main.js
+++ js/main.js
@@ -18,12 +18,12 @@ const lista = [
 
 let contenitoreElementi = document.getElementById('container-element')
 let li = document.createElement('li')
-
+let elemento = ""
 
 // Iterate usando un ciclo FOR per stampare ogni elemento in CONSOLE.
 for (let i = 0; i < lista.length; i++){
     //console.log(i)
-    let elemento = lista[i]
+     elemento = lista[i]
     console.log(i, elemento)
 
     //stampo in pagina
@@ -35,3 +35,28 @@ for (let i = 0; i < lista.length; i++){
 
 //Converto il ciclo FOR in un WHILE. Dovrete gestire il contatore manualmente.
 
+let i = 0
+
+while (i < lista.length){
+
+    //console.log(i)
+
+     elemento = lista[i]
+    //console.log(elemento)
+    
+    //Inserisco ogni elemento in pagina
+    // contenitoreElementi.innerHTML=(`<li>${elemento}</li>`)
+    // console.log(contenitoreElementi)
+
+
+    // metto dentro <li> elemento e poi li stampo n pagina dentro l'inner del contenitore
+    // li.innerText = (elemento)
+    // contenitoreElementi.innerHTML += (li)
+
+
+
+    i++
+
+}
+
+i=0
