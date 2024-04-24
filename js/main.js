663183504ae4d0163c6f3dcb0a9c24baf23b937c
francescofalcone05 <ciccio_97@live.it>
2024-04-24 18:56:43 +0300

    Bonus

--- js/main.js
+++ js/main.js
@@ -17,17 +17,19 @@ const lista = [
 
 
 let contenitoreElementi = document.getElementById('container-element')
-let li = document.createElement('li')
+const li = document.createElement('li')
 let elemento = ""
 
 // Iterate usando un ciclo FOR per stampare ogni elemento in CONSOLE.
 for (let i = 0; i < lista.length; i++){
     //console.log(i)
-     elemento = lista[i]
-    console.log(i, elemento)
+
+    
+    // elemento = lista[i]
+    // console.log(i, elemento)
 
     //stampo in pagina
-    contenitoreElementi.innerHTML=(`<li>${elemento}</li>`)
+    // contenitoreElementi.innerHTML+=(`<li>${elemento}</li>`)
 
 
 
@@ -41,17 +43,19 @@ while (i < lista.length){
 
     //console.log(i)
 
-     elemento = lista[i]
-    //console.log(elemento)
+    
+    elemento = lista[i]
+    console.log(i, elemento)
     
     //Inserisco ogni elemento in pagina
-    // contenitoreElementi.innerHTML=(`<li>${elemento}</li>`)
-    // console.log(contenitoreElementi)
+    contenitoreElementi.innerHTML +=(`<li>${elemento}</li>`)
+    console.log(contenitoreElementi)
 
 
-    // metto dentro <li> elemento e poi li stampo n pagina dentro l'inner del contenitore
-    // li.innerText = (elemento)
-    // contenitoreElementi.innerHTML += (li)
+    // metto dentro <li> elemento e poi li stampo n pagina dentro il contenitore usando APPEND
+    // li.innerText +=(elemento)
+    // contenitoreElementi.append( li )
+    
 
 
 
